b803b954b79de4470b804a46f29521801c6341af
prossellob <prosselloboix@gmail.com>
2017-09-19 17:07:31 +0200

    7kyu

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,4 +1,3 @@
-
 '''Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1.
 
 Replace certain values however if any of the following conditions are met:
