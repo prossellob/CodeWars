28c2bc3dc8f5b4c8ad115c843c3f8c863aaec223
prossellob <prosselloboix@gmail.com>
2017-09-21 16:47:34 +0200

    7kyu

---
+++ string_ends_with.py
@@ -0,0 +1,15 @@
+'''
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') # returns true
+solution('abc', 'd') # returns false
+
+'''
+
+def solution(string, ending):
+    if ending in string[len(string)-len(ending)::]:
+        return True
+    else:
+        return False
